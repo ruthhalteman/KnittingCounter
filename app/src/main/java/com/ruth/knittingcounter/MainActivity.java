83af4f28ebf23eb3c075642842d3dfd8bc3b20b3
rmwagner <rmwagner42@gmail.com>
2016-09-27 21:22:44 -0400

    super basic increment button yey

--- app/src/main/java/com/ruth/knittingcounter/MainActivity.java
+++ app/src/main/java/com/ruth/knittingcounter/MainActivity.java
@@ -4,13 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int rows;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rows = 0;
+
+        Button incrementButton = (Button) findViewById(R.id.plus);
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                rows = rows+1;
+                TextView rowDisplay = (TextView) findViewById(R.id.rows);
+                rowDisplay.setText(""+rows);
+            }
+        });
     }
 
     @Override

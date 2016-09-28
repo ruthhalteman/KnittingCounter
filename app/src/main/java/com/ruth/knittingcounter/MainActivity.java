b1967772fc0dd34832c98f9ecaa711bbd0945641
rmwagner <rmwagner42@gmail.com>
2016-09-28 11:03:37 -0400

    start at 1

--- app/src/main/java/com/ruth/knittingcounter/MainActivity.java
+++ app/src/main/java/com/ruth/knittingcounter/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rows = 0;
+        rows = 1;
 
         Button incrementButton = (Button) findViewById(R.id.plus);
         incrementButton.setOnClickListener(new View.OnClickListener() {

ffe82a7cb3bccfed63d51de159cff85131f0797b
rmwagner <rmwagner42@gmail.com>
2016-09-28 16:51:06 -0400

    edit counter name

--- app/src/main/java/com/ruth/knittingcounter/MainActivity.java
+++ app/src/main/java/com/ruth/knittingcounter/MainActivity.java
@@ -1,17 +1,23 @@
 package com.ruth.knittingcounter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int rows;
+    int current;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +25,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        rows = sharedPref.getInt("row", 1);
-        updateNumber();
+        current = sharedPref.getInt("current", 1);
+        rows = sharedPref.getInt(current + "_row", 1);
+        name = sharedPref.getString(current + "_name", "new project");
+        updateScreen();
 
         Button incrementButton = (Button) findViewById(R.id.plus);
         incrementButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 rows++;
-                updateNumber();
+                updateScreen();
 
                 SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putInt("row", rows);
+                editor.putInt(current+"_row", rows);
                 editor.apply();
             }
         });
     }
 
-    public void updateNumber() {
+    public void updateScreen() {
         TextView rowDisplay = (TextView) findViewById(R.id.rows);
-        rowDisplay.setText(""+rows);
+        rowDisplay.setText("" + rows);
+        TextView projectName = (TextView) findViewById(R.id.project);
+        projectName.setText(name);
     }
 
     @Override
@@ -60,16 +70,49 @@ public class MainActivity extends AppCompatActivity {
             rows = 1;
             SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putInt("row", rows);
+            editor.putInt(current+"_row", rows);
             editor.apply();
-            updateNumber();
+            updateScreen();
             return true;
         }
 
         if (id == R.id.add) {
+            editDialog();
+            return true;
+        }
+
+        if (id == R.id.edit) {
+            editDialog();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void editDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Edit your counter");
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View v = inflater.inflate(R.layout.edit_dialog, null);
+        builder.setView(v);
+
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                EditText newName = (EditText) v.findViewById(R.id.newName);
+                name = newName.getText().toString();
+                updateScreen();
+                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(current+"_name", name);
+                editor.apply();
+            }
+        });
+        builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
